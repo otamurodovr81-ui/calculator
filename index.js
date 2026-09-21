cf30e81ba4c8b693cbf0466a894c0c79660e0dad
otamurodovr81-ui <otamurodov81@gmail.com>
2026-09-21 20:23:53 +0500

    mm

---
+++ index.js
@@ -0,0 +1,80 @@
+const input = document.querySelector("#input");
+const btn = document.querySelectorAll("#btn");
+
+let lastValue = "";
+
+const operators = ["+", "-", "*", "/"];
+
+
+btn.forEach((itom) => {
+    itom.addEventListener("click", () => {
+        console.log(itom.value);
+
+        if (itom.value === "d") {
+            input.value = input.value.slice(0, -1);
+
+        } else if (itom.value === "C") {
+
+            input.value = "";
+            lastValue = "";
+
+        } else if (operators.includes(itom.value) && operators.includes(lastValue)) {
+            
+          return;
+            
+        } else if (itom.value === "=") {
+
+            console.log("teng bosld");
+            
+            const text = input.value;
+
+            if (text.includes("+")) {
+                const numbers = text.split("+");
+
+                const number1 = Number(numbers[0]);
+                const number2 = Number(numbers[1]);
+
+                const result = number1 + number2;
+
+                input.value = result;
+
+            } else if(text.includes("-")) {
+                const numbers = text.split("-");
+
+                const number1 = Number(numbers[0]);
+                const number2 = Number(numbers[1]);
+
+                const result = number1 - number2;
+
+                input.value = result;
+
+            } else if (text.includes("*")) {
+                const numbers = text.split("*");
+
+                const number1 = Number(numbers[0]);
+                const number2 = Number(numbers[1]);
+
+                const result = number1 * number2;
+
+                input.value = result;
+
+            } else if (text.includes("/")) {
+                const numbers = text.split("/");
+
+                const number1 = Number(numbers[0]);
+                const number2 = Number(numbers[1]);
+
+                const result = number1 / number2;
+
+                input.value = result;
+            } 
+        
+
+        } else {
+            input.value += itom.value;
+        }
+
+        lastValue = itom.value;
+        
+    });
+});
